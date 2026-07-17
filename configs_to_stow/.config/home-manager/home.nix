038f7fcc86324e6842e810b900ac4b58782016d1
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2026-07-18 00:16:44 +0530

    Add dark mode via home-manager (xsettingsd + GTK + dconf)
    - Add xsettingsd package and i3 startup entry for GTK theme propagation
    - Configure GTK dark theme (Mint-Y-Dark-Aqua) via home-manager gtk module
    - Set dconf color-scheme=prefer-dark and Nemo dark preference
    - Include pre-existing volume notification (dunstify) and dunst icon changes

--- configs_to_stow/.config/home-manager/home.nix
+++ configs_to_stow/.config/home-manager/home.nix
@@ -36,7 +36,43 @@
     # screen lock for i3
     # already installed system-wide as a dependency of i3 (apt)
     # i3lock
+
+    # gtk settings daemon for i3 (applies theme to gtk apps)
+    xsettingsd
   ];
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Mint-Y-Dark-Aqua";
+      package = pkgs.mint-y-icons;
+    };
+    iconTheme = {
+      name = "Mint-Y-Sand";
+      package = pkgs.mint-y-icons;
+    };
+    font = {
+      name = "Ubuntu";
+      size = 10;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Mint-Y-Dark-Aqua";
+      icon-theme = "Mint-Y-Sand";
+    };
+    "org/gnome/nemo/preferences" = {
+      theme-variant = "dark";
+    };
+  };
 
 #  programs.git.enable = true;
 #  programs.tmux.enable = true;
