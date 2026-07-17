5cbd198b63ff536ecf8a89f34c604b8c0bfa8509
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2026-07-17 15:33:59 +0530

    home-manager: add greenclip and clean up comments

--- configs_to_stow/.config/home-manager/home.nix
+++ configs_to_stow/.config/home-manager/home.nix
@@ -7,23 +7,24 @@
   home.stateVersion = "25.05";
 
   home.packages = with pkgs; [
+    # cli tools
     eza
     bat
 #   fd
 #   ripgrep
 
+    # rofi launcher
     rofi
 
-   # statusBar
-   i3blocks
+    # clipboard manager for rofi
+    haskellPackages.greenclip
 
-   # screenshot for i3
-   flameshot
-   #scrot
+    # status bar for i3
+    i3blocks
 
-
-  
-
+    # screenshot tool for i3
+    flameshot
+    #scrot
   ];
 
 #  programs.git.enable = true;
@@ -31,65 +32,4 @@
 
   programs.home-manager.enable = true;
 
-  programs.i3status-rust = {
-    enable = true;
-
-    bars.default = {
-      icons = "awesome6";
-      theme = "modern";
-
-      settings = {
-        theme.theme = "modern";
-        icons.icons = "awesome6";
-      };
-
-      blocks = [
-        {
-          block = "cpu";
-          interval = 1;
-          format = " CPU $utilization ";
-        }
-
-        {
-          block = "memory";
-          format = " RAM $mem_used.eng(prefix:Gi)/$mem_total.eng(prefix:Gi) ";
-        }
-
-        {
-          block = "load";
-          interval = 1;
-          format = " Load $1m ";
-        }
-
-        {
-          block = "disk_space";
-          path = "/";
-          format = " SSD $available.eng(prefix:Gi) ";
-        }
-
-        {
-          block = "net";
-          device = "wlp0s20f3";
-          interval = 2;
-          format = " WiFi $ssid ($signal_strength%) ";
-          format_alt = " ↑ $speed_up ↓ $speed_down ";
-        }
-
-        {
-          block = "battery";
-          format = " $percentage ";
-        }
-
-        {
-          block = "sound";
-        }
-
-        {
-          block = "time";
-          interval = 1;
-          format = " $timestamp.datetime(f:'%Y-%m-%d %H:%M:%S') ";
-        }
-      ];
-    };
-  };
 }
