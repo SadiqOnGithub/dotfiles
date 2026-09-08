0df3ab2527fdd1f204636dc5095009fe9d8bd0e8
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2026-09-08 16:18:30 +0530

    feat: add markdown preview to yazi

--- configs_to_stow/.config/home-manager/modules/yazi.nix
+++ configs_to_stow/.config/home-manager/modules/yazi.nix
@@ -1,14 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [ mdcat ];
+
   programs.yazi = {
     enable = true;
     enableBashIntegration = true;
+    plugins = {
+      piper = pkgs.yaziPlugins.piper;
+    };
     settings = {
       mgr = {
         show_hidden = true;
         sort_dir_first = true;
         ratio = [ 1 2 5 ];
+      };
+      plugin = {
+        prepend_previewers = [
+          { name = "*.md"; run = "piper -- mdcat --ansi --local --columns=\"$w\" \"$1\""; }
+          { name = "*.markdown"; run = "piper -- mdcat --ansi --local --columns=\"$w\" \"$1\""; }
+          { name = "*.mdown"; run = "piper -- mdcat --ansi --local --columns=\"$w\" \"$1\""; }
+          { name = "*.mkd"; run = "piper -- mdcat --ansi --local --columns=\"$w\" \"$1\""; }
+          { name = "*.mdx"; run = "piper -- mdcat --ansi --local --columns=\"$w\" \"$1\""; }
+        ];
       };
     };
   };
